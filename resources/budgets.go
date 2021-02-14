15395f48807e53697d917cd195e1643c04115430
Ken Keller <ken.keller.mp4k@statefarm.com>
2021-02-14 14:29:37 -0600

    WIP:service 'budgets' is global, but ses is not

--- resources/budgets.go
+++ resources/budgets.go
@@ -9,10 +9,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/sts"
 )
 
-func init() {
-	register("Budgets", ListBudgets)
-}
-
 type Budget struct {
 	svc  *budgets.Budgets
 	name *string
@@ -21,40 +17,47 @@ type Budget struct {
 
 // if there are no tags what should properties be?
 
+func init() {
+	register("Budgets", ListBudgets)
+}
+
 func ListBudgets(sess *session.Session) ([]Resource, error) {
 	svc := budgets.New(sess)
 
-	resources := []Resource{}
+	// resources := []Resource{}
 
-	// Lookup current account ID
 	fmt.Println("Entered Budgets Resource")
 
-	stsSvc := sts.New(sess)
-
-	callerID, err := stsSvc.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+	// Lookup current account ID
+	callerID, err := sts.New(sess).GetCallerIdentity(nil) //&sts.GetCallerIdentityInput{})
 	if err != nil {
 		return nil, err
 	}
 
-	accountID := callerID.Account
-	// fmt.Printf("account_num: %s \n", *accountID)
+	// accountID := callerID.Account
+	fmt.Printf("account_num: %s \n", *callerID.Account)
 
 	params := &budgets.DescribeBudgetsInput{
-		AccountId: aws.String(*accountID),
+		AccountId:  aws.String(*callerID.Account),
+		MaxResults: aws.Int64(100),
 	}
 
 	output, err := svc.DescribeBudgets(params)
 	if err != nil {
+		fmt.Printf("error: %s \n", err)
 		return nil, err
 	}
 
+	fmt.Printf("len=%d\n", len(output.Budgets))
+
 	for _, bud := range output.Budgets {
 		fmt.Println(bud.BudgetName)
-		resources = append(resources, &Budget{
-			svc:  svc,
-			name: bud.BudgetName,
-		})
+		// resources = append(resources, Budget{
+		// 	svc:  svc,
+		// 	name: bud.BudgetName,
+		// })
 	}
 
+	// return resources, nil
 	return nil, nil
 }
